21c08dc66c0d8cda6ec2202fae78a069296d10a0
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2026-07-21 11:58:35 -0400

    a

--- backend/review/models.py
+++ backend/review/models.py
@@ -4,8 +4,8 @@ from book.models import Book
 
 # Create your models here.
 class Review(models.Model):
-  user = models.ForeignKey(CustomUser, related_name="reviews", on_delete=models.CASCADE)
-  book = models.ForeignKey(Book, related_name="reviews", on_delete=models.CASCADE)
+  user = models.ForeignKey(CustomUser, related_name="user_reviews", on_delete=models.CASCADE)
+  book = models.ForeignKey(Book, related_name="book_reviews", on_delete=models.CASCADE)
   liked_by = models.ManyToManyField(CustomUser, related_name="liked_reviews", blank=True)
   written_text = models.CharField(max_length=300)
   star_rating = models.IntegerField(min=1, max=5, null=True)
