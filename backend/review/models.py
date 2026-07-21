30f886ce68c768b5e624c91c1022b7b0bdfa19f7
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2026-07-21 09:16:58 -0400

    eyad help me

--- backend/review/models.py
+++ backend/review/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user.models import CustomUser
+from book.models import Book
 
 # Create your models here.
+class Review(models.Model):
+  user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+  liked_by = models.ManyToManyField(CustomUser, related_name="liked_reviews", blank=True)
+  written_text = models.CharField(max_length=300)
+  star_rating = models.IntegerField(min=1, max=5, null=True)
+  spoiler = models.BooleanField(default=False)
+  approved = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f"{self.user}, {self.book}"
+    
